38ea65811e4abb8462db075c4a8265271a08372b
Ben <richterb3@gmail.com>
2016-04-12 02:32:00 -0700

    [Feature] myMatches view completed with accepted and pending filter dividers

--- www/js/services/matches.js
+++ www/js/services/matches.js
@@ -10,11 +10,13 @@ function matches($http, $state, Auth) {
 
   var potentialMatches = [];
   var all = [];
+  var myMatches = []; 
 
   return {
     getMatches: getMatches,// get all possible matches
     viewPotentialMatch: viewPotentialMatch,
-    confirmMatch: confirmMatch
+    confirmMatch: confirmMatch,
+    getMyMatches: getMyMatches
   };
 
   // ------------
@@ -32,12 +34,10 @@ function matches($http, $state, Auth) {
 
     function success(response) {
       // this is what response looks like:
-      console.log('get matches response ', response);
       // push in each match object to potential matches array
       response.data.results.forEach(function(match){
         potentialMatches.push(match);
       });
-      console.log('this is the potential matches array ', potentialMatches);
       return potentialMatches;
       
     }
@@ -53,7 +53,7 @@ function matches($http, $state, Auth) {
   }
 
   function confirmMatch(matchObj){
-    console.log('Match Accepted, this is the matchObj', matchObj);
+    console.log('Match Accepted, this is the matchObj post', matchObj);
     var request = {
       method: 'POST',
       url: 'http://localhost:8000/api/duos/find',
@@ -64,18 +64,50 @@ function matches($http, $state, Auth) {
       .then(success, error);
 
     function success(response){
-      console.log('this is the response line 67 sucess, ', response);
+      // console.log('this is the response line 67 sucess, ', response);
     }
     function error(response){
-      console.log('this is the response line 70 error, ', response);
+      // console.log('this is the response line 70 error, ', response);
     }
   }
 
-  // function getRandomInt(min, max){
-  //   return Math.floor(Math.random() *  (max - min + 1)) + min;
-  // }
+  function getMyMatches(){
+    var request = {
+      method: 'GET',
+      url: 'http://localhost:8000/api/duos/'
+    };
+    
+    return $http(request)
+      .then(success, error);
+
+    function success(response) {
+      // this is what response looks like:
+      console.log('get matches response ', response);
+      response.data.results.forEach(function(myMatch){
+        myMatches.push(myMatch);
+      });
+      return myMatches;
+      // push in each match object to potential matches array
+      // response.data.results.forEach(function(match){
+      //   potentialMatches.push(match);
+      // });
+      // console.log('this is the potential matches array ', potentialMatches);
+      // return potentialMatches;
+      
+    }
+
+    function error(response) {
+      // TODO: error handling 
+      console.log('error in myMatches ', response );
+    }
+  }
 
 
+/*
+  function getRandomInt(min, max){
+    return Math.floor(Math.random() *  (max - min + 1)) + min;
+  }
+*/
 
 } 
 
