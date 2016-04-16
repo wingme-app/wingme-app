200459b5013001d8a852eb4c18b8b6b7515aeebe
Kan Adachi <Kadachi1023@gmail.com>
2016-04-16 09:41:03 -0700

    [Bugfix] module.exports for db helpers sorted

--- modules/helpers.js
+++ modules/helpers.js
@@ -41,66 +41,8 @@ Counter.prototype.on = function(limit, cb) {
   this.cb = cb;
 };
 
-/**
- *  Generate a 'real' status: something that reveals who gave consent first in a pending relationship.
- *
- */
-function realStatus(duo, clientID) {
-  // boolean, if the client is the first user (uID1) in our duos join table.
-  var clientToTarget = duo.u1ID === clientID || duo.uID1 === clientID;
-  var statusInDatabase = duo.status;
-  var result;
-
-  switch (statusInDatabase) {
-    case 'duosPen':
-      result = clientToTarget ? 'pendingWing' : 'bePendingWing';
-      break;
-    case 'duosAcc':
-      result = 'isWing';
-      break;
-    case 'duosCurPen':
-      console.log('duo = ', duo);
-      console.log('clientToTarget = ', clientToTarget);
-      result = clientToTarget ? 'pendingCurrentWing' : 'beCurrentWing';
-      break;
-    case 'duosCurAcc':
-      result = 'isCurrentWing';
-      break;
-    case 'duosRej':
-      result = 'rejected';
-      break;
-    default:
-      throw 'status invalid! duo = ' + duo;
-  }
-
-  return result;
-}
-
-/**
- *  Filter out the current user from a duo.
- *
- */
- function filterDuo(duo, clientID) {
-   // keep it pure
-   var newUser = {};
-
-   if (duo.u1ID === clientID) {
-     newUser.ID = duo.u2ID;
-     newUser.firstname = duo.u2fn;
-     newUser.lastname = duo.u2ln;
-   } else {
-     newUser.ID = duo.u1ID;
-     newUser.firstname = duo.u1fn;
-     newUser.lastname = duo.u1ln;
-   }
-
-   return newUser;
- }
-
 
 module.exports = {
   sendJSON: sendJSON,
-  Counter: Counter,
-  realStatus: realStatus,
-  filterDuo: filterDuo
+  Counter: Counter
 }
